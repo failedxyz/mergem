af18f30be5e4dbcfd57f1226fab58ac26050540c
ko-tori <littletori0912@gmail.com>
2017-01-29 15:16:37 +0900

    tint

--- game.js
+++ game.js
@@ -7,6 +7,28 @@ var imageLibrary = imageLibrary || {};
 const GWIDTH = 1280, GHEIGHT = 720;
 var zoom = 0.6;
 
+var tint = function (img, col) {
+    if (col === undefined) return img;
+    var canvas = document.createElement('canvas');
+    canvas.height = img.height;
+    canvas.width = img.width;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    var w = img.width,
+        h = img.height;
+    if (!w || !h) return img;
+    var imgdata = ctx.getImageData(0, 0, w, h);
+    var rgba = imgdata.data;
+    //console.log(imgdata.data);
+    for (var px = 0, ct = w * h * 4; px < ct; px += 4) {
+        rgba[px] *= col[0] / 255;
+        rgba[px + 1] *= col[1] / 255;
+        rgba[px + 2] *= col[2] / 255;
+    }
+    ctx.putImageData(imgdata, 0, 0);
+    return canvas;
+};
+
 class LevelMap {
     static parse(mapdata, metadata) {
         var lines = mapdata.split(/\r?\n/g).map(function (line) { return line.replace(/~+$/, ""); }).filter(function (line) { return line.length > 0; });
