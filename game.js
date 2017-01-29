3149baa0cec772075cc48093f6e0bb24eb55a41d
ko-tori <littletori0912@gmail.com>
2017-01-29 20:13:44 +0900

    asdf

--- game.js
+++ game.js
@@ -355,6 +355,7 @@ var loadLevel = function (level) {
     rawCtx = rawCanvas.getContext("2d");
     for (var j = 0; j < levels[ci].metadata.control.length; ++j) {
         controlled |= (1 << levels[ci].metadata.control[j]);
+        levels[ci].map.characters[levels[ci].metadata.control[j]] = true;
     }
 };
 
